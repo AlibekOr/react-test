f1651bdf346f4941f0e07024ada77a0d29b86571
Ali <alibekorakbaev3132@gmail.com>
2023-09-18 13:16:54 +0500

    useReduser

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
 import React from 'react'
-import UseStates from './components/hooks/UseStates'
-import States from './components/hooks/States'
-import EffectOne from './components/hooks/EffectOne'
-import Props from './components/props/Props'
-import UseRef from './components/hooks/UseRef'
-import Email from './components/hooks/Email'
-import Scroll from './components/hooks/Scroll'
-import UseMemos from './components/hooks/UseMemos'
-import Memo from './components/useMemo/Memo'
-import UseColbeck from './components/hooks/UseColbeck'
-import Callback1 from './components/useMemo/Callback1'
-import Callback2 from './components/useMemo/Callback2'
-import Callback3 from './components/useMemo/Callback3'
-import Callback4 from './components/useMemo/Callback4'
-import Memo2 from './components/useMemo/Memo2'
-import Memo3 from './components/useMemo/Memo3'
-import Memo4 from './components/useMemo/Memo4'
-import Callback5 from './components/useMemo/Callback5'
-import Memo5 from './components/useMemo/Memo5'
-import Memo6 from './components/useMemo/Memo6'
-import Memo7 from './components/useMemo/Memo7'
+import UseReduser from './components/useReduser/UseReduser'
 const App = () => {
     return (
         <div className='con'>
+            <UseReduser />
             {/* <UseStates />
             <States />
             <EffectOne />
@@ -41,8 +22,8 @@ const App = () => {
             <Memo3 />
             <Memo4 />*/}
             {/* <Callback5 /> */}
-            <Memo6 />
-            <Memo7 />
+            {/* <Memo6 />
+            <Memo7 /> */}
         </div>
     )
 }
